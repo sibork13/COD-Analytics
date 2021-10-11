f409ad3e4d72c8c42ecc6aea628338f3de86ff3c
Oscar Herrera <oscarherrerafigueroa13@gmail.com>
2021-10-11 14:14:24 -0500

    Changed how scrape general matches, now extract every match block

--- functions.py
+++ functions.py
@@ -68,25 +68,47 @@ class HomePage(COD_Tracker):
         name = self._extract_one(self._xpath_dir['player_name_xpath']).text
         return name
 
-    @property
-    def matches_list(self):
-        listmatch = self._extract_all(self._xpath_dir['match_list_page'])
-        return listmatch
-    @property
-    def get_place(self):
-        return self._extract_all(self._xpath_dir['place'])
 
-    @property
-    def get_game_mode(self):
-        return self._extract_all(self._xpath_dir['game_mode'])
-
-    @property
-    def get_loby_kd(self):
-        return self._extract_all(self._xpath_dir['loby_kd'])
-
-    @property
-    def get_kills(self):
-        return self._extract_all(self._xpath_dir['kills'])
+    def matches_list(self,allowed_game_mode_list):
+        # list of all data will be stored ordenated
+        list = []
+        #List of every match link
+        match_List = []
+        #getting bloc of matches per date
+        matches_per_date = self._extract_all(self._xpath_dir['match_per_date'])
+        for date in matches_per_date:
+            listmatch = date.find_elements_by_xpath(self._xpath_dir['listmatch'])
+            for match in listmatch:
+                place = match.find_elements_by_xpath(self._xpath_dir['place'])
+                game_mode = match.find_elements_by_xpath(self._xpath_dir['game_mode'])
+                loby_kd = match.find_elements_by_xpath(self._xpath_dir['loby_kd'])
+                kills = match.find_elements_by_xpath(self._xpath_dir['kills'])
+                match_link = [a_tag.get_attribute("href") for a_tag in match.find_elements_by_xpath('.//a[@class="match-row__link"]')]
+                for index in range(0,len(place)):
+                    if game_mode[index].text in allowed_game_mode_list:
+                        # print(game_mode[index].text)
+                        #here will separate data for match
+                        match_List.append(match_link[index])
+                        list.append(place[index].text)
+                        list.append(game_mode[index].text)
+                        list.append(loby_kd[index].text)
+                        list.append(kills[index].text)
+        return list,match_List
+    # @property
+    # def get_place(self):
+    #     return self._extract_all(self._xpath_dir['place'])
+    #
+    # @property
+    # def get_game_mode(self):
+    #     return self._extract_all(self._xpath_dir['game_mode'])
+    #
+    # @property
+    # def get_loby_kd(self):
+    #     return self._extract_all(self._xpath_dir['loby_kd'])
+    #
+    # @property
+    # def get_kills(self):
+    #     return self._extract_all(self._xpath_dir['kills'])
 
     @property
     def click_next(self):
@@ -94,9 +116,9 @@ class HomePage(COD_Tracker):
         button.click()
         time.sleep(5)
 
-    @property
-    def get_main_match(self):
-        return self._extract_href(self._xpath_dir['a_tag'])
+    # @property
+    # def get_main_match(self):
+    #     return self._extract_href(self._xpath_dir['a_tag'])
 
     @property
     def close_connection(self):
@@ -107,9 +129,9 @@ class MatchPage(COD_Tracker):
     def __init__(self,web_page,url):
         super().__init__(web_page,url)
 
-    @property
-    def extract_players_stats(self):
-        return self._extract_all(self._xpath_dir['player'])
+    # @property
+    # def extract_players_stats(self):
+    #     return self._extract_all(self._xpath_dir['player'])
 
     @property
     def extract_butons(self):
@@ -134,10 +156,9 @@ class MatchPage(COD_Tracker):
         stats_list = []
         for i_bloc in bloc:
             test_stats = i_bloc.find_elements_by_xpath(self._xpath_dir['player'])
+            test_enemies = i_bloc.find_elements_by_xpath(self._xpath_dir['enemy_name'])
             for j in range(0,len(test_stats)):
                 if test_stats[j].text in allowed_metrics:
                     stats_list.append(test_stats[j].text)
                     stats_list.append(test_stats[j+1].text)
-        # for jj in range(0,len(stats_list),10):
-        #     print(stats_list[jj:jj+10])
-        return stats_list
+        return stats_list,test_enemies
--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def match_info(website,url):
     buttons_list = match_page.extract_butons
     for button in buttons_list:
         match_page.click_open_stats(button)
-    players_stats = match_page.test_get_player_block
+    players_stats,players_name = match_page.test_get_player_block
     match_page.close_connection
+    player_name_index = 0
     for future_row in range(0,len(players_stats),10):
+        print(players_name[player_name_index])
         print(players_stats[future_row:future_row+10])
+        print("*********************")
+        player_name_index += 1
 
 
 
@@ -33,11 +37,11 @@ if __name__== '__main__':
     jugador_id = page.player_name
     print(jugador_id)
     print("\n")
-    game_mode = page.get_game_mode
-    place = page.get_place
-    loby_kd = page.get_loby_kd
-    kills_and_kd = page.get_kills
-    every_match_link_list = page.get_main_match
+    # game_mode = page.get_game_mode
+    # place = page.get_place
+    # loby_kd = page.get_loby_kd
+    # kills_and_kd = page.get_kills
+    # every_match_link_list = page.get_main_match
 
 
     '''
@@ -48,31 +52,38 @@ if __name__== '__main__':
     with open('Game_Mode.txt',mode='r') as f:
         allowed_game_mode = list(f.read().split(",\n"))
 
-    # creating a list of good indexes to scrape match per match after
-    good_index_list = []
+    all_matches_list,every_matrch_link_list = page.matches_list(allowed_game_mode)
 
-    # filtering matches, only allow game mode matches
-    for index_list in range(0,len(place)):
-        list = []
-        if game_mode[index_list].text in allowed_game_mode:
-            good_index_list.append(index_list)
-            list.append(game_mode[index_list].text)
-            list.append(place[index_list].text)
-            list.append(loby_kd[loby_kd_indx].text)
-            list.append(kills_and_kd[index_list*2].text)
-            list.append(kills_and_kd[index_list*2+1].text)
-            # print("Partida")
-            # print(list)
-            loby_kd_indx += 1
-        else:
-            pass
-            # print("NO SUEM NADA")
+
+
+    # for i in range(0,len(all_matches_list),4):
+    #     print(all_matches_list[i:i+4])
+
+    # # creating a list of good indexes to scrape match per match after
+    # good_index_list = []
+    # # filtering matches, only allow game mode matches
+    # for index_list in range(0,len(place)):
+    #     list = []
+    #     if game_mode[index_list].text in allowed_game_mode:
+    #         good_index_list.append(index_list)
+    #         list.append(game_mode[index_list].text)
+    #         list.append(place[index_list].text)
+    #         list.append(loby_kd[loby_kd_indx].text)
+    #         list.append(kills_and_kd[index_list*2].text)
+    #         list.append(kills_and_kd[index_list*2+1].text)
+    #         print("Partida")
+    #         print(list)
+    #         loby_kd_indx += 1
+    #     else:
+    #         pass
+    #
 
     page.close_connection
-    for i in good_index_list[0:1]:
-        print(i)
+    for link in every_matrch_link_list[0:5]:
+        # print(i)
         print('*****************INFO DE LA PARTIDA *********************')
-        match_info(website,every_match_link_list[i])
+        print(link)
+        # match_info(website,link)
 
 
     print("FIN DE LA EJECUCION")
